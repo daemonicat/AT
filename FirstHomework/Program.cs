d98dd9ac527009dff4f247159d215aa359c3b487
daemonicat <dima62131@gmail.com>
2023-11-02 19:01:09 +0300

    Test try

---
+++ FirstHomework/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        var username = Console.ReadLine();
+        Console.WriteLine("Hello, {0}!", username);
+    }
+}
